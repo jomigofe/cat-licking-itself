02d33be38942e26f192805883de039bd6a4f7124
José Ferreira <jose@someofmy.work>
2022-01-18 21:17:56 -0500

    Added rocky images following mouse going crazy

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,34 @@
 import ReactPlayer from "react-player";
+import { useEffect, useRef } from "react";
+import useMouse from "@react-hook/mouse-position";
+
+const imageList = [
+  "/images/signal-2022-01-18-205223_001.jpeg",
+  "/images/signal-2022-01-18-205223_003.jpeg",
+  "/images/signal-2022-01-18-205223_004.jpeg",
+  "/images/signal-2022-01-18-205223_005.jpeg",
+  "/images/signal-2022-01-18-205223_006.jpeg",
+  "/images/signal-2022-01-18-205223_007.jpeg",
+  "/images/signal-2022-01-18-205223_008.jpeg",
+  "/images/signal-2022-01-18-205223_009.jpeg",
+];
+
+//function that returns a random number between 0 and the length of the imageList array
+function randomNumber(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 export default function Home() {
+  const ref = useRef(null);
+
+  const mouse = useMouse(ref);
+
+  useEffect(() => {
+    console.log(mouse);
+  }, [mouse]);
+
   return (
-    <div className="player-wrapper">
+    <div ref={ref} className="player-wrapper">
       <ReactPlayer
         url="/signal-2022-01-18-171220_001.mp4"
         muted={true}
@@ -13,6 +39,16 @@ export default function Home() {
         width="100%"
         height="100%"
       />
+      <div
+        className="image-wrapper"
+        style={{
+          top: `${mouse.y}px`,
+          left: `${mouse.x}px`,
+          "--background-image": `url(${
+            imageList[randomNumber(imageList.length)]
+          })`,
+        }}
+      ></div>
     </div>
   );
 }
