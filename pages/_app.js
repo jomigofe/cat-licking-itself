929aa59c2cd72b67998308258370552a7e9aedd3
José Ferreira <jose@someofmy.work>
2022-01-19 10:41:13 -0500

    added ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
+import ua from "universal-analytics";
+
 import "../styles/styles.scss";
 
 function MyApp({ Component, pageProps }) {
+  var visitor = ua("G-8FRPKV3DCJ");
+
   return <Component {...pageProps} />;
 }
 
